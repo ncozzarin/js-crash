ee0acea403708b34244b3d8f4a46b2dd41aca45c
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-07-02 09:57:50 -0300

    Project creation

---
+++ home.js
@@ -0,0 +1,33 @@
+console.log('hello');
+
+//alert('yoo');
+
+// Variables
+
+//var b = 'smoothie';
+//console.log(b);
+
+
+// var age = prompt('What is your age?');
+// document.getElementById('someText').innerHTML = age ;
+
+
+// numbers in Javascript
+
+var num1 = 5.7;
+console.log(6*25);
+
+// Functions
+
+function name()
+{
+    var name = prompt('What is your name?');
+    var result = 'Hello' + ' ' + name;
+    console.log(result);
+}
+
+name();
+
+// Array
+
+let fruits = ['banana', 'pera','manzana']
